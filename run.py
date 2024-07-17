79450a40e932dc944a9491f9a1ec5e24a22d59e6
Nico Van Der Walt <nvanderwalt12@yahoo.com>
2024-07-17 12:10:44 +0000

    Initial commit with board setup

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+#Score tracking 
+scores = {"computer": 0, "player": 0} 
+
+
+#The __init__ method sets up a new game board with the specified size, number of ships and initializes it with empty guesses and ship placements. 
+class Board:
+    def __init__(self, size, num_ships, name, type): 
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
