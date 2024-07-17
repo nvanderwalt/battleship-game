a02ee8e50856d3d01e500fbbace4685f60707486
Nico Van Der Walt <nvanderwalt12@yahoo.com>
2024-07-17 12:23:03 +0000

    Create for loop to iterate over each row and print

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from random import randint
 scores = {"computer": 0, "player": 0} 
 
 
-#The __init__ method sets up a new game board with the specified size, number of ships and initializes it with empty guesses and ship placements. 
+#The __init__ method sets up a new game board with the specified size, number of ships and initializes it with empty guesses and ship placements.
+# IMPORTANT - Code was taken from the video of Code Institute-Project 3
 class Board:
     def __init__(self, size, num_ships, name, type): 
         self.size = size
@@ -14,3 +15,8 @@ class Board:
         self.type = type
         self.guesses = []
         self.ships = []
+
+#This defines a method named print that belongs to the Board class.
+    def print(self):
+        for row in self.board: #This is a for loop that iterates over each row in the board.
+            print(" ".join(row)) #To print this string to the console, effectively printing the row with spaces between the cells for better readability.
